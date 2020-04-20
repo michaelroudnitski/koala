cc0b6ae1305d17a9f9b96375347bc7070728f0b0
Michael <michaelroudnitski@gmail.com>
2020-04-19 23:19:49 -0400

    recaptcha -> honeypot

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import SectionHeader from "../Misc/SectionHeader";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { CheckCircle, Send } from "react-feather";
 
 const encode = (data) => {
   return Object.keys(data)
@@ -13,6 +14,7 @@ function ContactForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [submit, setSubmit] = useState(false);
 
   const handleSubmit = (e) => {
     fetch("/", {
@@ -20,7 +22,9 @@ function ContactForm() {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", name, email, message }),
     })
-      .then(() => alert("Success!"))
+      .then(() => {
+        setSubmit(true);
+      })
       .catch((error) => alert(error));
 
     e.preventDefault();
@@ -29,40 +33,47 @@ function ContactForm() {
   return (
     <div>
       <SectionHeader title="Contact Me" />
-      <Form onSubmit={handleSubmit}>
-        <Form.Row className="justify-content-between">
-          <Form.Group className="col-md-4 col-12">
-            <Form.Label>Your Name</Form.Label>
+      {!submit ? (
+        <Form onSubmit={handleSubmit}>
+          <Form.Row className="justify-content-between">
+            <Form.Group className="col-md-4 col-12">
+              <Form.Label>Your Name</Form.Label>
+              <Form.Control
+                type="text"
+                name="name"
+                placeholder="John Doe"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="col-md-8 col-12">
+              <Form.Label>Your Email</Form.Label>
+              <Form.Control
+                type="email"
+                name="email"
+                placeholder="jdoe@gmail.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
+          </Form.Row>
+          <Form.Group>
+            <Form.Label>Your Message</Form.Label>
             <Form.Control
-              type="text"
-              placeholder="John Doe"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
+              as="textarea"
+              rows="3"
+              name="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
             />
           </Form.Group>
-          <Form.Group className="col-md-8 col-12">
-            <Form.Label>Your Email</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="jdoe@gmail.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
-        </Form.Row>
-        <Form.Group>
-          <Form.Label>Your Message</Form.Label>
-          <Form.Control
-            as="textarea"
-            rows="3"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+          <Button variant="primary" type="submit">
+            Submit <Send size={18} />
+          </Button>
+        </Form>
+      ) : (
+        <CheckCircle className="m-auto" color={"green"} />
+      )}
     </div>
   );
 }
