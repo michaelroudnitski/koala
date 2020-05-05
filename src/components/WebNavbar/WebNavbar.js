7c0311c83cb000a66f3abb07f76a49e848ac489f
Michael Roudnitski <mroudnitski@Michaels-MacBook-Pro.local>
2020-05-05 13:19:02 -0400

    add wordpress

--- src/components/WebNavbar/WebNavbar.js
+++ src/components/WebNavbar/WebNavbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
-// import Nav from "react-bootstrap/Nav";
+import Nav from "react-bootstrap/Nav";
 
 import "./WebNavbar.scss";
 
@@ -8,10 +8,9 @@ function WebNavbar() {
   return (
     <Navbar>
       <Navbar.Brand href="/">MR</Navbar.Brand>
-      {/* <Nav className="ml-auto">
-        <Nav.Link href="#home">My Work</Nav.Link>
-        <Nav.Link href="#link">Contact</Nav.Link>
-      </Nav> */}
+      <Nav className="ml-auto">
+        <Nav.Link to="/blog">Blog</Nav.Link>
+      </Nav>
     </Navbar>
   );
 }
---
+++ wp/wp-config.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wordpress');
+
+/** MySQL database username */
+define( 'DB_USER', 'wordpress');
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'wordpress');
+
+/** MySQL hostname */
+define( 'DB_HOST', 'db:3306');
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '853f60856316348623922f8a7715b755080d853b');
+define( 'SECURE_AUTH_KEY',  '62f93d0376b0dad2887269492277b5986e94883b');
+define( 'LOGGED_IN_KEY',    'a2cdbd76bbe94d20d00bad0e5a8ded2b62e457f8');
+define( 'NONCE_KEY',        'c0a6e4fcea387001689a63410fb5afcde3ebe0df');
+define( 'AUTH_SALT',        'c907864902a659ee05d837cebb1e1b239cc4aa53');
+define( 'SECURE_AUTH_SALT', 'f78312dc3cc86e5423f01615079f2b48ff540420');
+define( 'LOGGED_IN_SALT',   'effaa3578328411f71ace0f945d4bee1a1fd7bba');
+define( 'NONCE_SALT',       'f786a8bda79587ef5076a8c653b3f84b147ec4e8');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+// If we're behind a proxy server and using HTTPS, we need to alert WordPress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
