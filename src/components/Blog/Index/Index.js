1af2356822384b41b9a447f6061ede487f6767a6
Michael Roudnitski <mroudnitski@Michaels-MacBook-Pro.local>
2020-05-05 22:14:04 -0400

    looping through posts

--- src/components/Blog/Index/Index.js
+++ src/components/Blog/Index/Index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
+import Spinner from "react-bootstrap/Spinner";
 
 function Index() {
 
@@ -12,16 +13,21 @@ function Index() {
         fetch(`${process.env.REACT_APP_WP_API}/posts`)
             .then((res) => res.json())
             .then((data) => {
-                console.log({data})
                 setPosts({ requestComplete: true, data });
             })
             .catch(() => setPosts({ requestComplete: true }));
     }, []);
 
+    let content = null;
+    if (!posts.requestComplete) {
+        content = <Spinner role="status" animation="border" />;
+    } else if (posts.data) {
+        return posts.data.map((post, index) => <h1>{post.title.rendered}</h1>);
+    }
+
     return (
         <Container>
-            <h1>Hello world</h1>
-            {posts.requestComplete ? "hello" : "bye"}
+            {content}
         </Container>
     );
 }
