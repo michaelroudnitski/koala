73d7bf61b0500190ec76673a37a87c055669dfb3
Michael Roudnitski <mroudnitski@ibm.com>
2020-05-19 14:40:08 -0400

    add meta tags to posts

--- src/components/Blog/Post/Post.js
+++ src/components/Blog/Post/Post.js
@@ -6,6 +6,7 @@ import moment from "moment";
 import readingTime from "reading-time";
 import featuredImgSrc from "../../../util/featuredImgSrc";
 import { Link } from "react-router-dom";
+import MetaTags from "react-meta-tags";
 import "./Post.scss";
 
 function Post() {
@@ -21,7 +22,6 @@ function Post() {
       .then((data) => {
         data = data[0];
         setPost({ requestComplete: true, data });
-        document.title = data.title.rendered;
       })
       .catch(() => setPost({ requestComplete: true }));
   }, [postSlug]);
@@ -42,6 +42,12 @@ function Post() {
     const timeToRead = readingTime(post.data.content.rendered);
     content = (
       <Col className="post mx-auto">
+        <MetaTags>
+          <title>{post.data.title.rendered}</title>
+          <meta name="description" content="Blog | Michael Roudnitski" />
+          <meta property="og:title" content={post.data.title.rendered} />
+          <meta property="og:image" content={featuredImgSrc(post.data)} />
+        </MetaTags>
         <Link to="/blog">Back to posts</Link>
         <hr />
         <h1 className="text-capitalize blog-title">
